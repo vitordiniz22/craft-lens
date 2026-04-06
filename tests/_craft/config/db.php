e509761f1e3a333ecea00b92fc6d9c51a411609c
Vitor Diniz <vitordiniz22@gmail.com>
2026-04-06 13:35:25 +1200

    Fix charset mismatch by setting utf8mb4 in test database config

--- tests/_craft/config/db.php
+++ tests/_craft/config/db.php
@@ -6,4 +6,6 @@ return [
     'password' => getenv('CRAFT_DB_PASSWORD') ?: getenv('DB_PASSWORD'),
     'schema' => getenv('CRAFT_DB_SCHEMA') ?: getenv('DB_SCHEMA'),
     'tablePrefix' => getenv('CRAFT_DB_TABLE_PREFIX') ?: getenv('DB_TABLE_PREFIX'),
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
 ];
